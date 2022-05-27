a40448b787185aaae2730f19eb8e92066880589e
idanstr <strulovich@mail.tau.ac.il>
2022-05-28 01:22:47 +0300

    Copied the mongo database mello_db and connected to my own atlas acount, and seperated dev and prod environments for mongo

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,8 @@
 module.exports = {
-    'dbURL': 'mongodb+srv://omere94:1111@mello.7htyb.mongodb.net/mello_db?retryWrites=true&w=majority',
+    'dbURL': 'mongodb+srv://clegane47:14ZFMTDkMIMtZQ2q@cluster0.shgds.mongodb.net/?retryWrites=true&w=majority',
     // 'dbURL': process.env.MONGODB_URI,
 }
+
+// mongodump --uri mongodb+srv://clegane47:<PASSWORD>@cluster0.shgds.mongodb.net/<DATABASE> 
+// mongodump --uri mongodb+srv://omere94:1111@mello.7htyb.mongodb.net/mello_db
+//mongorestore --uri mongodb+srv://clegane47:14ZFMTDkMIMtZQ2q@cluster0.shgds.mongodb.net 
