04b6b9d5590cc8c0bd7211acc46895deecb24794
idanstr <strulovich@mail.tau.ac.il>
2022-06-05 20:03:37 +0300

    Deployment ready

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,3 @@
 module.exports = {
-    'dbURL': 'mongodb+srv://clegane47:14ZFMTDkMIMtZQ2q@cluster0.shgds.mongodb.net/?retryWrites=true&w=majority',
-    // 'dbURL': process.env.MONGODB_URI,
+    'dbURL': process.env.MONGODB_URI,
 }
-
-// mongodump --uri mongodb+srv://clegane47:<PASSWORD>@cluster0.shgds.mongodb.net/<DATABASE> 
-// mongodump --uri mongodb+srv://omere94:1111@mello.7htyb.mongodb.net/mello_db
-//mongorestore --uri mongodb+srv://clegane47:14ZFMTDkMIMtZQ2q@cluster0.shgds.mongodb.net 
