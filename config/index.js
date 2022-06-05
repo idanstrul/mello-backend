b99bec6969e9901d37d989d9b10105af63df7658
idanstr <strulovich@mail.tau.ac.il>
2022-06-05 23:21:01 +0300

    Bug fix

--- config/index.js
+++ config/index.js
@@ -6,6 +6,7 @@ console.log('process.env.NODE_ENV ===', process.env.NODE_ENV)
 if (process.env.NODE_ENV === 'production') {
     // we are in production - return the prod set of keys
     config = require('./prod')
+    console.log('config:', config)
 } else {
     // we are in development - return the dev keys!!!
     // config = require('./prod')
